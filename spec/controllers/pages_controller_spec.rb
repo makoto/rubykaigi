d26475b49432f777c5e7802695f8deb9e4ea30a3
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-08-03 19:14:29 +0900

    [#3687414][WIP] spec only. what should i do next?
    - 次にcontrollerのテストでどう攻めたらいいかわからない今日この頃

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PagesController do
-  # this spec auto generated
-  it { true.should be_true }
+  context "page does not found" do
+    specify do
+      get :show, :year => RubyKaigi.latest_year, :year => '2010', :page_name => "registration", :locale => 'ja'
+      response.should render_template("/pages/2010/registration") # こんなtemplateはほんとうは存在してない
+      response.status.should == 404 # 200……だとッ?!
+    end
+  end
 end
