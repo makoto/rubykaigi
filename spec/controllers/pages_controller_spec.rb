fd4aa1f0b05aded88bfafc5746c8ac04d61c1334
Keita Urashima <ursm@ursm.jp>
2010-08-03 20:07:44 +0900

    [#3687414] assert raise ActionView::MissingTemplate
    コントローラのレベルではこのぐらいしかできないような

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,10 +3,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PagesController do
   context "page does not found" do
+    integrate_views
+
     specify do
-      get :show, :year => RubyKaigi.latest_year, :year => '2010', :page_name => "registration", :locale => 'ja'
-      response.should render_template("/pages/2010/registration") # こんなtemplateはほんとうは存在してない
-      response.status.should == 404 # 200……だとッ?!
+      expect {
+        get :show, :year => '2010', :page_name => 'registration', :locale => 'ja'
+      }.to raise_error(ActionView::MissingTemplate)
     end
   end
 end
